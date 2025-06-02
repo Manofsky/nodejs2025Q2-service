4257c78cdffe9002aa253cd6b8e4ded281e18217
manofsky <clegrof@gmail.com>
2025-06-02 23:46:04 +0300

    feat: enhance API documentation for App and Users controllers with Swagger annotations

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -8,30 +8,76 @@ import {
   Delete,
   HttpCode,
 } from '@nestjs/common';
+import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdatePasswordDto } from './dto/update-password.dto';
 
+@ApiTags('Users')
 @Controller('user')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Get()
+  @ApiOperation({ summary: 'Get all users' })
+  @ApiResponse({
+    status: 200,
+    description: 'Return all users',
+  })
   findAll() {
     return this.usersService.findAll();
   }
 
   @Get(':id')
+  @ApiOperation({ summary: 'Get user by id' })
+  @ApiResponse({
+    status: 200,
+    description: 'Return user by id',
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'Bad request. userId is invalid (not uuid)',
+  })
+  @ApiResponse({
+    status: 404,
+    description: 'User not found',
+  })
   findById(@Param('id') id: string) {
     return this.usersService.findOne(id);
   }
 
   @Post()
+  @ApiOperation({ summary: 'Create new user' })
+  @ApiResponse({
+    status: 201,
+    description: 'User has been created',
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'Bad request. body does not contain required fields',
+  })
   create(@Body() dto: CreateUserDto) {
     return this.usersService.create(dto);
   }
 
   @Put(':id')
+  @ApiOperation({ summary: 'Update user password' })
+  @ApiResponse({
+    status: 200,
+    description: 'Password has been updated',
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'Bad request. userId is invalid (not uuid)',
+  })
+  @ApiResponse({
+    status: 403,
+    description: 'Old password is wrong',
+  })
+  @ApiResponse({
+    status: 404,
+    description: 'User not found',
+  })
   updatePassword(@Param('id') id: string, @Body() dto: UpdatePasswordDto) {
     return this.usersService.updatePassword(
       id,
@@ -42,6 +88,19 @@ export class UsersController {
 
   @Delete(':id')
   @HttpCode(204)
+  @ApiOperation({ summary: 'Delete user' })
+  @ApiResponse({
+    status: 204,
+    description: 'User has been deleted',
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'Bad request. userId is invalid (not uuid)',
+  })
+  @ApiResponse({
+    status: 404,
+    description: 'User not found',
+  })
   remove(@Param('id') id: string) {
     this.usersService.remove(id);
     return;
